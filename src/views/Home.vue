5b32e8fa1f8a85ff9288df7bde8becb19f06ed9c
MENNJ <120314622+MENNJ@users.noreply.github.com>
2024-07-09 14:08:41 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,19 @@
 <template>
-<Nva_bar/>
-  <div ref="photos" class="photos dark:bg-blue-100" >
-    <div ref="photos" class="photos_line" v-for="(line, index) in lines" :key="index">
-      <div ref="photos_line_photo" class="photos_line_photo bg-white dark:bg-slate-800 ring-1 ring-slate-900/5
-        shadow-xl" v-for="(photo, photoIndex) in line" :key="photoIndex">
+  <Nva_bar />
+  <div ref="photos" class="photos dark:bg-blue-100">
+    <div class="photos_line" v-for="(line, index) in lines" :key="index">
+      <div class="photos_line_photo bg-white dark:bg-slate-800 ring-1 ring-slate-900/5 shadow-xl" v-for="(photo, photoIndex) in line" :key="photoIndex">
         <Starport :port="String(photo.title)" style="width:100%; height:70%;">
-        <img :src="photo.imgSrc" />
-         </Starport>
-
-        <router-link  :to="{
-          path:'/home2',
-          query:{
-            img:photo.imgSrc,
-            title:photo.title
-          }}"
+          <img :src="photo.imgSrc" />
+        </Starport>
+        <router-link
+            :to="{ path:'/home2', query:{ img:photo.imgSrc, title:photo.title }}"
             type="button"
-            class="bg-[#e8e8e8] text-center w-48 rounded-2xl h-14 relative font-sans text-black text-xl font-semibold group dark:bg-black dark:text-white"
-        >
-          <div
-              class="bg-green-400 max-w-full rounded-xl h-12 w-1/4  absolute left-1 top-1 group-hover:w-[184px] z-10 duration-500 "
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="32" viewBox="0 0 448 512"><path fill="currentColor" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h306.7L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            class="bg-[#e8e8e8] text-center w-48 rounded-2xl h-14 relative font-sans text-black text-xl font-semibold group dark:bg-black dark:text-white">
+          <div class="bg-green-400 max-w-full rounded-xl h-12 w-1/4 absolute left-1 top-1 group-hover:w-[184px] z-10 duration-500">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="32" viewBox="0 0 448 512">
+              <path fill="currentColor" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h306.7L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+            </svg>
           </div>
           <p class="mt-3.5 ml-4 dark:text-white">Go Back</p>
         </router-link>
@@ -31,162 +24,121 @@
 </template>
 
 <script setup>
-
-  import { Starport } from "vue-starport"
-</script>
-
-
-<script >
+import { Starport } from "vue-starport";
+import { ref, onMounted } from 'vue';
 import { gsap } from 'gsap';
-
 import pubUse from '../utils/pub-use';
 
-export default {
-  name: 'home',
+const lines = ref([
+  Array.from({ length: 8 }, (_, i) => ({ imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: `Apple Air_Max${i + 1}` })),
+  Array.from({ length: 8 }, (_, i) => ({ imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: `Apple Air_Max${i + 9}` })),
+  Array.from({ length: 8 }, (_, i) => ({ imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: `Apple Air_Max${i + 17}` })),
+  Array.from({ length: 8 }, (_, i) => ({ imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: `Apple Air_Max${i + 25}` })),
+]);
 
-  data() {
-    return {
-      lines: [
-        [
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max1' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max2' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max3' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max4' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max5' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max6' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max7' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max8' },
-        ],
-        [
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max9' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max10' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max11' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max12' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max13' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max14' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max15' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max16' },
-        ],
-        [
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max17' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max18' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max19' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max20' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max21' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max22' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max23' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max24' },
-        ],
-        [
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max25' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max26' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max27' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max28' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max29' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max30' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max31' },
-          { imgSrc: `${pubUse.getAssetsFile('Air_Max.png')}`, title: 'Apple Air_Max32' },
-        ],
-      ],
-    };
-  },
-  mounted(){
-    let imgs = this.$refs.photos_line_photo;
-    let Sheet;
-    console.log(imgs);
-    console.log(this);
+const photos = ref(null);
 
-    const photobox = {
-      container : this.$refs.photos,
-      img_data: [],
-      container_width: 0,//容器的宽度
-      container_height: 0,//容器的高度
-      photo_width: 0,//图片的宽度
-      photo_height: 0,//图片的高度
-      if_movable: false,//如果活动
-      mouse_x: 0,//鼠标x
-      mouse_y: 0,//鼠标y
-      standard_width: 1440,//标准宽度
-      scale_nums: 1,
-      init() {
-        this.resize();
-        window.addEventListener("resize", () => {
-          this.resize();
+onMounted(() => {
+  const photobox = {
+    container: photos.value,
+    imgData: [],
+    containerWidth: 0,
+    containerHeight: 0,
+    photoWidth: 0,
+    photoHeight: 0,
+    ifMovable: false,
+    mouseX: 0,
+    mouseY: 0,
+    standardWidth: 1440,
+    scaleNums: 1,
+    init() {
+      this.resize();
+      window.addEventListener("resize", this.resize.bind(this));
+      this.container.addEventListener("mousedown", this.onMouseDown.bind(this));
+      this.container.addEventListener("mouseup", this.onMouseUp.bind(this));
+      this.container.addEventListener("mouseleave", this.onMouseUp.bind(this));
+      this.container.addEventListener("mousemove", this.onMouseMove.bind(this));
+      this.container.addEventListener("touchstart", this.onTouchStart.bind(this));
+      this.container.addEventListener("touchend", this.onMouseUp.bind(this));
+      this.container.addEventListener("touchmove", this.onTouchMove.bind(this));
+    },
+    resize() {
+      const imgs = [...document.querySelectorAll(".photos_line_photo")];
+      this.containerWidth = this.container.offsetWidth;
+      this.containerHeight = this.container.offsetHeight;
+      this.photoWidth = imgs[0].offsetWidth;
+      this.photoHeight = imgs[0].offsetHeight;
+      this.scaleNums = document.body.offsetWidth / this.standardWidth;
+      this.container.style.transform = `scale(${this.scaleNums})`;
+      gsap.to(imgs, { transform: `translate(0,0)`, duration: 0, ease: 'power4.out' });
+      this.imgData = imgs.map(img => ({
+        node: img,
+        x: img.offsetLeft,
+        y: img.offsetTop,
+        movX: 0,
+        movY: 0,
+        ani: 0
+      }));
+    },
+    onMouseDown(event) {
+      this.ifMovable = true;
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
+    },
+    onMouseUp() {
+      this.ifMovable = false;
+    },
+    onMouseMove(event) {
+      this.move(event.clientX, event.clientY);
+    },
+    onTouchStart(event) {
+      this.ifMovable = true;
+      this.mouseX = event.touches[0].clientX;
+      this.mouseY = event.touches[0].clientY;
+    },
+    onTouchMove(event) {
+      this.move(event.touches[0].clientX, event.touches[0].clientY);
+    },
+    move(x, y) {
+      if (!this.ifMovable) return;
+      const distanceX = (x - this.mouseX) / this.scaleNums;
+      const distanceY = (y - this.mouseY) / this.scaleNums;
+      this.imgData.forEach(img => {
+        let duration = 1;
+        img.movX += distanceX;
+        if (img.x + img.movX > this.containerWidth) {
+          img.movX -= this.containerWidth;
+          duration = 0;
+        }
+        if (img.x + img.movX < -this.photoWidth) {
+          img.movX += this.containerWidth;
+          duration = 0;
+        }
+        img.movY += distanceY;
+        if (img.y + img.movY > this.containerHeight) {
+          img.movY -= this.containerHeight;
+          duration = 0;
+        }
+        if (img.y + img.movY < -this.photoHeight) {
+          img.movY += this.containerHeight;
+          duration = 0;
+        }
+        if (img.ani) img.ani.kill();
+        img.ani = gsap.to(img.node, {
+          transform: `translate(${img.movX}px,${img.movY}px)`,
+          duration: duration,
+          ease: 'power4.out'
         });
-        this.container.addEventListener("mousedown", () => {
-          this.if_movable = true;
-          this.mouse_x = event.clientX;
-          this.mouse_y = event.clientY;
-        });
-        this.container.addEventListener("mouseup", () => {
-          this.if_movable = false;
-        });
-
-        this.container.addEventListener("mouseleave", () => {
-          this.if_movable = false;
-        });
-
-        this.container.addEventListener("mousemove", () => {
-          this.move(event.clientX, event.clientY);
-        });
-      },
-      resize() {
-        this.container_width = this.container.offsetWidth;
-        this.container_height = this.container.offsetHeight;
-        this.photo_width = imgs[0].offsetWidth;
-        this.photo_height = imgs[0].offsetHeight;
-        this.scale_nums = document.body.offsetWidth / this.standard_width;
-        this.container.style.transform = `scale(${this.scale_nums})`;
-        gsap.to(imgs, {
-          transform: `translate(0,0)`, duration: 0, ease: 'power4.out'
-        });
-        this.img_data = [];
-
-        imgs.forEach(img => {
-          this.img_data.push({
-            node: img, x: img.offsetLeft, y: img.offsetTop, mov_x: 0, mov_y: 0, ani: 0
-          });
-        });
-      },
-      move(x, y) {
-        if (!this.if_movable) return 0;
-        let distance_x = (x - this.mouse_x) / this.scale_nums;
-        let distance_y = (y - this.mouse_y) / this.scale_nums;
-        this.img_data.forEach((img) => {
-          let duration = 1;
-          img.mov_x += distance_x;
-          if (img.x + img.mov_x > this.container_width) {
-            img.mov_x -= this.container_width;
-            duration = 0;
-          }
-          if (img.x + img.mov_x < -this.photo_width) {
-            img.mov_x += this.container_width;
-            duration = 0;
-          }
-          img.mov_y += distance_y;
-          if (img.y + img.mov_y > this.container_height) {
-            img.mov_y -= this.container_height;
-            duration = 0;
-          }
-          if (img.y + img.mov_y < -this.photo_height) {
-            img.mov_y += this.container_height;
-            duration = 0;
-          }
-          if (img.ani) img.ani.kill();
-          img.ani = gsap.to(img.node, {
-            transform: `translate(${img.mov_x}px,${img.mov_y}px)`, duration: duration, ease: 'power4.out'
-          });
-        });
-        this.mouse_x = x;
-        this.mouse_y = y;
-      }
-    };
-    photobox.init();
-  },
-};
+      });
+      this.mouseX = x;
+      this.mouseY = y;
+    }
+  };
+  photobox.init();
+});
 </script>
-<style>
 
+<style>
 * {
   padding: 0;
   margin: 0;
@@ -194,7 +146,7 @@ export default {
 
 img {
   width: 100%;
-  height:  auto;
+  height: auto;
   pointer-events: none;
   user-select: none;
   z-index: 99999;
@@ -217,8 +169,6 @@ body {
   overflow: hidden;
 }
 
-
-
 /* homecss */
 .photos {
   position: absolute;
@@ -232,7 +182,6 @@ body {
   margin-bottom: 48em;
   flex-shrink: 0;
 }
-
 
 .photos_line_photo {
   display: flex;
@@ -253,7 +202,6 @@ body {
   transition: 1s;
 }
 
-
 .photos_line_photo h2 {
   font-size: 20px;
   transition: 0.6s;
@@ -263,30 +211,26 @@ body {
 }
 
 .photos_line_photo:hover h2 {
-  z-index: 1;
+  z-index:1;
   opacity: 1;
   transform: scale(1) translateY(-320%);
 }
 
-
-
-.photos_line_photo button:active{
+.photos_line_photo button:active {
   box-shadow: 4px 4px 12px #c5c5c5, -4px -4px 12px #ffffff;
 }
 
 @media screen and (max-aspect-ratio: 1.5/1) {
-
-  .photos_line,
-  .photos_line_photo {
+  .photos_line, .photos_line_photo {
     font-size: 2px;
   }
 }
 
 @media screen and (max-aspect-ratio: 0.8/1) {
-  .photos_line,
-  .photos_line_photo {
+  .photos_line, .photos_line_photo {
     font-size: 2.8px;
   }
 }
+
 
 </style>
